3e3addde281866f39f2e60c7deabfccdd6d4fcb7
hitton0096 <stardust.rain.memory@gmail.com>
2024-01-20 23:29:22 +0900

    Modify:更新個所と更新する値を入力するテキストボックスを追加

--- widget/main_window.py
+++ widget/main_window.py
@@ -26,24 +26,42 @@ class MainWindow(QMainWindow):
 
         # top_widget = QWidget()
         # top_widget_layout = QVBoxLayout(top_widget)
+        # 画面レイアウトは以下のサイトを参考に作成
+        # https://stackoverflow.com/questions/12007807/create-qt-layout-with-fixed-height
         base_layout = QVBoxLayout()
-        select_sheet_label = QLabel("更新するシートファイルを選択してください。")
+        select_dir_label = QLabel("更新するファイルが入っているディレクトリを選択してください。")
 
-
-        self.line_edit = QLineEdit()
-        self.line_edit.setReadOnly(True)
+        self.select_dir_edit = QLineEdit()
+        self.select_dir_edit.setReadOnly(True)
         button = QPushButton("参照")
         button.clicked.connect(self.the_button_was_clicked)
 
+        cell_address_label = QLabel("更新するセル番地を入力してください。")
+        cell_address_edit = QLineEdit()
+        cell_address_edit.setText("")
+        cell_address_edit.setFixedWidth(40)
+        cell_address_edit.setPlaceholderText("A1")
+        # 英数字で大文字に自動変換
+        cell_address_edit.setInputMask(">NNNNN")
+
+        update_value_label = QLabel("更新する値を入力してください。")
+        update_value_edit = QLineEdit()
+        update_value_edit.setText("")
+
         select_layout = QHBoxLayout()
-        select_layout.addWidget(self.line_edit)
+        select_layout.addWidget(self.select_dir_edit)
         select_layout.addWidget(button)
 
-        base_layout.addWidget(select_sheet_label)
+        base_layout.addWidget(select_dir_label)
         base_layout.addLayout(select_layout)
+        base_layout.addWidget(cell_address_label)
+        base_layout.addWidget(cell_address_edit)
+        base_layout.addWidget(update_value_label)
+        base_layout.addWidget(update_value_edit)
         # addStretchを入れることで空白分を埋めるというか伸ばす
         base_layout.addStretch(1)
         # top_widget.setFixedHeight(20)
+
 
         central_widget = QWidget()
         central_widget.setLayout(base_layout)
@@ -64,7 +82,7 @@ class MainWindow(QMainWindow):
             # selectedFilter,
             options
         )
-        self.line_edit.setText(fileNames)
+        self.select_dir_edit.setText(fileNames)
         print(fileNames)
 
     def the_button_was_toggled(self, checked):
