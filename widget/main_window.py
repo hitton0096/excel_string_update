609f1f4690cfb9c7a29b49a47273c23ba75def57
hitton0096 <stardust.rain.memory@gmail.com>
2024-01-20 20:51:01 +0900

    modify:画面レイアウトを修正

--- widget/main_window.py
+++ widget/main_window.py
@@ -24,43 +24,29 @@ class MainWindow(QMainWindow):
         # print(active_sheet.cell(column=1, row=1).value)
         # print(active_sheet['A1'].value)
 
-        # button1 = QPushButton('1')
-        # button2 = QPushButton('2')
-        # button3 = QPushButton('3')
-        
-        # layout = QGridLayout()
-        # # レイアウトにウィジェットを追加
-        # layout.addWidget(button1, 0, 0, 1, 2)
-        # layout.addWidget(button2, 1, 0)
-        # layout.addWidget(button3, 1, 1)
+        # top_widget = QWidget()
+        # top_widget_layout = QVBoxLayout(top_widget)
+        base_layout = QVBoxLayout()
+        select_sheet_label = QLabel("更新するシートファイルを選択してください。")
 
-        # central_widget = QWidget()
-        # central_widget.setLayout(layout)
-        # self.setCentralWidget(central_widget)
-
-        g_layout = QGridLayout()
-        dir_select_label = QLabel("更新するシートファイルを作成してください。")
-        # style = 'background-color: rgb(255, 50, 50);'
-        # dir_select_label.setStyleSheet(style)
-        # 列結合する場合に行結合しなくても最小値の1を設定する必要がある
-        g_layout.addWidget(dir_select_label, 0, 0, 1, 2)
 
         self.line_edit = QLineEdit()
         self.line_edit.setReadOnly(True)
-        g_layout.addWidget(self.line_edit, 1, 0, alignment=Qt.AlignmentFlag.AlignTop)
-
         button = QPushButton("参照")
         button.clicked.connect(self.the_button_was_clicked)
-        g_layout.addWidget(button, 1, 1, alignment=Qt.AlignmentFlag.AlignTop)
 
-        # g_layout.setRowStretch(0, 0)
-        # g_layout.setRowStretch(1, 3)
-        # g_layout.setRowMinimumHeight(0, 10)
-        # これを入れないと0行目の高さが大きくなるため無理やり高さを出して確保
-        g_layout.setRowMinimumHeight(1, 60)
+        select_layout = QHBoxLayout()
+        select_layout.addWidget(self.line_edit)
+        select_layout.addWidget(button)
+
+        base_layout.addWidget(select_sheet_label)
+        base_layout.addLayout(select_layout)
+        # addStretchを入れることで空白分を埋めるというか伸ばす
+        base_layout.addStretch(1)
+        # top_widget.setFixedHeight(20)
 
         central_widget = QWidget()
-        central_widget.setLayout(g_layout)
+        central_widget.setLayout(base_layout)
         self.setCentralWidget(central_widget)
 
     def the_button_was_clicked(self):
